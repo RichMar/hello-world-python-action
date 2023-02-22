f5d3c1e76610ab7ddd0e906fc5729876950d4f47
RM <30491118+RichMar@users.noreply.github.com>
2023-02-22 10:49:51 +0100

    Sekret

--- src/get_num_square.py
+++ src/get_num_square.py
@@ -1,15 +1,5 @@
-# src/get_num_square.py
 import os
 
-# get the input and convert it to int
-num = os.environ.get("INPUT_NUM")
-if num:
-    try:
-        num = int(num)
-    except Exception:
-        exit('ERROR: the INPUT_NUM provided ("{}") is not an integer'.format(num))
-else:
-    num = 1
+input_variable = os.environ['REPO_SECRET']
 
-# to set output, print to shell in following syntax
-print(f"::set-output name=num_squared::{num ** 2}")
+print("Input Variable:", input_variable)
