f650d4bc6637d73923de5e4c0534bcb475bd4dfe
RM <30491118+RichMar@users.noreply.github.com>
2023-02-22 11:53:40 +0100

    sekret06

--- src/get_num_square.py
+++ src/get_num_square.py
@@ -1,5 +1,6 @@
 import os
 
 input_variable = os.environ['ENV_SECRET']
-
+input_variable2 = os.environ['REPO_SECRET']
 print("Input Variable:", input_variable)
+print("Input Variable2:", input_variable2)
